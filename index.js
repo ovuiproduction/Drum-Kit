9c749d33c27f38cb3c53e89c3cae4095f1db0a39
ONKAR ANIL WAGHMODE <136163415+ovuiproduction@users.noreply.github.com>
2023-08-11 19:18:25 +0530

    Drum Kit Project

---
+++ index.js
@@ -0,0 +1,69 @@
+var totalbutton = document.querySelectorAll(".drum").length;
+
+
+for(let i = 0;i<totalbutton;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
+
+        var buttoninnerhtml = this.innerHTML;
+        diffsound(buttoninnerhtml);
+        buttonAnimation(buttoninnerhtml);
+    });
+}
+
+function diffsound(buttoninnerhtml){
+
+    switch(buttoninnerhtml){
+
+        case 'w':
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default :
+         console.log(buttoninnerhtml);
+         break;
+    }
+}
+
+document.addEventListener("keypress" ,function(event){
+
+    diffsound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+function buttonAnimation(currentKey){
+
+    var activeButton = document.querySelector("."+currentKey);
+    // document.querySelectorAll(".drum").classList.remove("pressed");
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+
+        activeButton.classList.remove("pressed");
+    } ,100);
+}
